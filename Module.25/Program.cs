ff34b083c2eec85230872f3e2092da5005dd7b10
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-06-29 16:36:51 +0300

    В Main создали экземпляр LibraryContext, добавили пользователей в БД и сохранили изменения

--- Module.25/Program.cs
+++ Module.25/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Module._25;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new LibraryContext())
+        {
+            // Создание пользователей
+            var user1 = new User { Name = "Иван", Email = "ivan@gmail.com" };
+            var user2 = new User { Name = "Анна", Email = "anna@example.com" };
+            context.Users.AddRange(user1, user2);
+
+            // Создание книг
+            var book1 = new Book { Title = "Война и мир", Year = 1869 };
+            var book2 = new Book { Title = "Преступление и наказание", Year = 1866 };
+            context.Books.AddRange(book1, book2);
+
+            //  Сохранение изменений
+            context.SaveChanges();
+        }
+
+        Console.WriteLine("Данные успешно сохранены в базе данных.");
+        Console.ReadLine();
+    }
+}
